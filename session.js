19ec3caa729d5528a3af364f7e8ff4faa53be23b
sergiy <aserg1234@yandex.ru>
2019-10-02 11:35:55 +0300

    class Session was added

--- session.js
+++ session.js
@@ -2,7 +2,7 @@
 
 const storage = require('./storage.js');
 
-const TOKEN_LENGTH = 32;//в реальном ставить 80 или больше
+const TOKEN_LENGTH = 80;//в реальном ставить 80 или больше
 const ALPHA_UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const ALPHA_LOWER = 'abcdefghijklmnopqrstuvwxyz';
 const ALPHA = ALPHA_UPPER + ALPHA_LOWER;
@@ -18,3 +18,54 @@ const generateToken = () => {
   }
   return key;
 };
+
+class Session extends Map {
+  constructor(token) {
+    super();
+    this.token = token;
+  }
+
+  static start(client) {
+    if (client.session) return client.session;
+    const token = generateToken();
+    client.token = token;
+    const session = new Session(token);
+    client.session = session;
+    client.setCookie('token', token);
+    storage.set(token, session);
+    return session;
+  }
+
+  static restore(client) {
+    const { cookie } = client;
+    if (!cookie) return;
+    const sessionToken = cookie.token;
+    if (sessionToken) {
+      return new Promise((resolve, reject) => {
+        storage.get(sessionToken, (err, session) => {
+          if (err) reject(new Error('No session'));
+          Object.setPrototypeOf(session, Session.prototype);
+          client.token = sessionToken;
+          client.session = session;
+          resolve(session);
+        });
+      });
+    }
+  }
+
+  static delete(client) {
+    const { token } = client;
+    if (token) {
+      client.deleteCookie('token');
+      client.token = undefined;
+      client.session = null;
+      storage.delete(token);
+    }
+  }
+
+  save() {
+    storage.save(this.token);
+  }
+}
+
+module.exports = Session;
