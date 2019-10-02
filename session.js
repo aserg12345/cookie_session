106be8ecff8050747b717c769caf49bb911e946b
sergei <antoninaSobaka@example.com>
2019-10-02 08:51:05 +0300

    first

---
+++ session.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const storage = require('./storage.js');
+
+const TOKEN_LENGTH = 32;//в реальном ставить 80 или больше
+const ALPHA_UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const ALPHA_LOWER = 'abcdefghijklmnopqrstuvwxyz';
+const ALPHA = ALPHA_UPPER + ALPHA_LOWER;
+const DIGIT = '0123456789';
+const ALPHA_DIGIT = ALPHA + DIGIT;
